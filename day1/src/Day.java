a9a53fa24ad890302798337feab26bf65d85039c
Sumanth K R. <sumanth.kr@excelindia.com>
2024-04-29 06:46:11 -0700

    backend

---
+++ day1/src/Day.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Day {
+
+	public static void main(String[] args) {
+		 int[]arr= {1,2,5,6,7};
+		
+		 
+		 	for( int i=0; i<arr.length; i++) {
+		 
+		   System.out.println(arr[i]); 	}
+ 	}
+}
+		
+		
+
---
+++ day1/src/Patren.java
@@ -0,0 +1,25 @@
+
+public class Patren {
+
+	public static void main(String[] args) {
+		 int num=6;
+		 for(int i=1; i<=num; i++)
+		 {
+			 for(int j=1; j<=num; j++)
+			 
+			 {
+				 
+				 System.out.print(j +" ");
+			 }
+		 
+			 System.out.println();
+		 }
+			}
+
+		}
+//1 2 3 4 5 6 
+//1 2 3 4 5 6 
+//1 2 3 4 5 6 
+//1 2 3 4 5 6 
+//1 2 3 4 5 6 
+//1 2 3 4 5 6 
